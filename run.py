2f57babc677e5582ed2fa98919eed184513b9844
Tianshu <tianshu2@andrew.cmu.edu>
2022-04-05 13:40:50 -0400

    Wrong import

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 """Run and test runtime."""
 
 from silverline import _run, parse
-from utils import parse_args
 
 if __name__ == '__main__':
-    args = parse_args(parse.http, parse.mqtt, parse.benchmark)
+    args = parse.parse_args(parse.http, parse.mqtt, parse.benchmark)
     _run._main(args)
