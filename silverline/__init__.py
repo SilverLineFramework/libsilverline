688adf62e0f9fb8835ae84b72389fb0d79a182ef
Tianshu <tianshu2@andrew.cmu.edu>
2022-04-07 15:07:20 -0400

    Add missing import (data module)

--- silverline/__init__.py
+++ silverline/__init__.py
@@ -1,6 +1,7 @@
 """Silverline Python Library."""
 
 from . import client
+from . import data
 # from . import runtime
 
 from . import parse
@@ -11,6 +12,6 @@ from . import _stop_runtimes
 
 __all__ = [
     "parse", "parse_args",
-    "client",  # runtime,
+    "client", "data",  # runtime,
     "_run", "_stop_runtimes"
 ]
