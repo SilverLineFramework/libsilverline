bad8f9440823dd2be20c3103c4b28eae02dd7454
Tianshu <tianshu2@andrew.cmu.edu>
2022-04-07 14:38:22 -0400

    Made session support multiple directories.

--- silverline/data/session.py
+++ silverline/data/session.py
@@ -11,20 +11,31 @@ class Session:
 
     Parameters
     ----------
-    dir : str
+    dir : str or str[]
         Base directory for this session. Should contain a `manifest.json`
         and directories file-1, file-2, ... for each trace.
     """
 
     def __init__(self, dir="data"):
 
+        if isinstance(dir, str):
+            dir = [dir]
         self.dir = dir
-        with open(os.path.join(dir, "manifest.json")) as f:
-            self.manifest = json.load(f)
+
+        self.manifest = {"runtimes": {}, "modules": {}, "files": {}}
+        for d in dir:
+            with open(os.path.join(dir, "manifest.json")) as f:
+                manifest = json.load(f)
+                self.manifest["runtimes"].update(manifest["runtimes"])
+                self.manifest["modules"].update(manifest["modules"])
+                self.manifest["files"].update({
+                    k: os.path.join(d, "file-{}".format(v))
+                    for k, v in manifest["files"].items()
+                })
+
         self.runtimes = list(set(
             v for _, v in self.manifest["runtimes"].items()))
         self.files = [v for _, v in self.manifest["files"]]
-
         self.traces = {}
 
     def get(self, file):
@@ -32,25 +43,17 @@ class Session:
 
         Parameters
         ----------
-        file : str or int
-            File name or id. If str, looks up the file id through the manifest.
-            If int, uses as a file id directly.
+        file : str
+            File name or path to trace. If passed as a file name, looks up
+            the file name through the manifest. Otherwise, uses the path
+            directly.
         """
-        if isinstance(file, str):
-            file = self.manifest["files"][file]
-
+        file = self.manifest["files"].get(file, file)
         if file not in self.traces:
             try:
                 self.traces[file] = Trace(
-                    dir=os.path.join(self.dir, "file-{}".format(file)),
-                    manifest=self.manifest)
+                    dir=os.path.join(self.dir, file), manifest=self.manifest)
             except FileNotFoundError:
                 self.traces[file] = None
 
         return self.traces[file]
-
-    def matrix(self, func, mat, **kwargs):
-        """Apply function to a runtimes x files matrix."""
-        for i, rt in self.runtimes:
-            for j, file in self.files:
-                mat[i][j] = func(mat, rt, file, **kwargs)
